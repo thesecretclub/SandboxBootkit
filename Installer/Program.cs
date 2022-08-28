42647d4b5aa9505d8778da3d986f56eaab29fe31
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2022-08-28 13:14:19 +0200

    Pause on exceptions

--- Installer/Program.cs
+++ Installer/Program.cs
@@ -88,66 +88,77 @@ namespace Installer
             var whoResult = Exec("whoami");
             if (whoResult.Output.ToLowerInvariant().Contains("system"))
             {
-                Console.WriteLine("Running as system!");
-
-                var programData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-                var baseImages = Path.Combine(programData, "Microsoft", "Windows", "Containers", "BaseImages");
-                if (!Directory.Exists(baseImages))
-                    Error($"Directory not found: {baseImages}");
-
-                var guid = Directory.EnumerateDirectories(baseImages).FirstOrDefault();
-                if (guid == null)
-                    Error($"BaseImages GUID not found, install & start Windows Sandbox");
-
-                var baseLayer = Path.Combine(guid, "BaseLayer");
-                if (!Directory.Exists(baseLayer))
-                    Error($"Directory found: {baseLayer}, install & start Windows Sandbox");
-
-                var bootPath = Path.Combine(baseLayer, "Files", "EFI", "Microsoft", "Boot");
-                if (!Directory.Exists(bootPath))
-                    Error($"Directory not found: {bootPath}, install & start Windows Sandbox");
-
-                var bootmgfwPath = Path.Combine(bootPath, "bootmgfw.efi");
-                if (!File.Exists(bootmgfwPath))
-                    Error($"File not found: {bootmgfwPath}, install & start Windows Sandbox");
-
-                var backupName = "bootmgfw.bak";
-                var bootmgfwBakPath = Path.Combine(bootPath, backupName);
-                if (!File.Exists(bootmgfwBakPath))
+                try
                 {
-                    Console.WriteLine($"Creating backup of bootmgfw.efi -> {backupName}");
-                    File.Copy(bootmgfwPath, bootmgfwBakPath);
+                    Console.WriteLine("Running as system!");
+
+                    var programData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+                    var baseImages = Path.Combine(programData, "Microsoft", "Windows", "Containers", "BaseImages");
+                    if (!Directory.Exists(baseImages))
+                        Error($"Directory not found: {baseImages}");
+
+                    var guid = Directory.EnumerateDirectories(baseImages).FirstOrDefault();
+                    if (guid == null)
+                        Error($"BaseImages GUID not found, install & start Windows Sandbox");
+
+                    var baseLayer = Path.Combine(guid, "BaseLayer");
+                    if (!Directory.Exists(baseLayer))
+                        Error($"Directory found: {baseLayer}, install & start Windows Sandbox");
+
+                    var bootPath = Path.Combine(baseLayer, "Files", "EFI", "Microsoft", "Boot");
+                    if (!Directory.Exists(bootPath))
+                        Error($"Directory not found: {bootPath}, install & start Windows Sandbox");
+
+                    var bootmgfwPath = Path.Combine(bootPath, "bootmgfw.efi");
+                    if (!File.Exists(bootmgfwPath))
+                        Error($"File not found: {bootmgfwPath}, install & start Windows Sandbox");
+
+                    var backupName = "bootmgfw.bak";
+                    var bootmgfwBakPath = Path.Combine(bootPath, backupName);
+                    if (!File.Exists(bootmgfwBakPath))
+                    {
+                        Console.WriteLine($"Creating backup of bootmgfw.efi -> {backupName}");
+                        File.Copy(bootmgfwPath, bootmgfwBakPath);
+                    }
+
+                    var backupPath = Path.Combine(basePath, backupName);
+                    if (!File.Exists(backupPath))
+                        File.Copy(bootmgfwBakPath, backupPath);
+
+                    var bootkitPath = Path.Combine(basePath, "bootmgfw.efi");
+                    if (!File.Exists(bootkitPath))
+                    {
+                        var sandboxBootkit = Path.Combine(basePath, "SandboxBootkit.efi");
+                        if (!File.Exists(sandboxBootkit))
+                            Error($"Bootkit not found ${sandboxBootkit}, please compile SandboxBootkit");
+
+                        var python = FindPython();
+                        if (python == null)
+                            Error($"Failed to find python 3");
+
+                        var injector = Path.Combine(basePath, "injector.py");
+                        // TODO: redirect stderr?
+                        var pythonResult = Exec(python, $"\"{injector}\" \"{backupPath}\" \"{sandboxBootkit}\"");
+                        Console.WriteLine(pythonResult.Output);
+                        if (pythonResult.ExitCode != 0)
+                            Error($"Failed to inject bootkit!\n" + pythonResult.Output.Trim());
+                    }
+
+                    File.Copy(bootkitPath, bootmgfwPath, true);
+
+                    Console.WriteLine("Bootkit installed: " + bootmgfwPath);
+
+                    Console.WriteLine("\nPress any key to exit...");
+                    Console.ReadKey();
                 }
-
-                var backupPath = Path.Combine(basePath, backupName);
-                if (!File.Exists(backupPath))
-                    File.Copy(bootmgfwBakPath, backupPath);
-
-                var bootkitPath = Path.Combine(basePath, "bootmgfw.efi");
-                if (!File.Exists(bootkitPath))
+                catch (Exception x)
                 {
-                    var sandboxBootkit = Path.Combine(basePath, "SandboxBootkit.efi");
-                    if (!File.Exists(sandboxBootkit))
-                        Error($"Bootkit not found ${sandboxBootkit}, please compile SandboxBootkit");
+                    Console.WriteLine($"Exception: {x}\n");
+                    Console.WriteLine(x.StackTrace);
 
-                    var python = FindPython();
-                    if (python == null)
-                        Error($"Failed to find python 3");
-
-                    var injector = Path.Combine(basePath, "injector.py");
-                    // TODO: redirect stderr?
-                    var pythonResult = Exec(python, $"\"{injector}\" \"{backupPath}\" \"{sandboxBootkit}\"");
-                    Console.WriteLine(pythonResult.Output);
-                    if (pythonResult.ExitCode != 0)
-                        Error($"Failed to inject bootkit!\n" + pythonResult.Output.Trim());
+                    Console.WriteLine("\nPress any key to exit...");
+                    Console.ReadKey();
                 }
-
-                File.Copy(bootkitPath, bootmgfwPath, true);
-
-                Console.WriteLine("Bootkit installed: " + bootmgfwPath);
-
-                Console.WriteLine("\nPress any key to exit...");
-                Console.ReadKey();
             }
             else
             {
